0338e3fa2a0a3fff25832b83b1182311abf45c48
cimpoes.andrei.alexandru@gmail.com <cimpoes.andrei.alexandru@gmail.com>
2019-01-05 16:47:42 +0200

    Add Person class to Library(Board)

--- BoardApplication/Program.cs
+++ BoardApplication/Program.cs
@@ -3,32 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BoardApplication;
 
 namespace BoardApplication
 {
     class Program
     {
+        private static void DisplayMenu()
+        {
+
+            Console.WriteLine("What do you want to do?");
+
+            Console.WriteLine("1 - Display all users");
+
+            Console.WriteLine("2 - Add a new user");
+
+            Console.WriteLine("3 - Get a user by message");
+
+            Console.WriteLine("4 - Delete a user by message");
+
+            Console.WriteLine("5 - Update a user");
+
+            Console.WriteLine("6 - Exit");
+
+        }
         static void Main(string[] args)
         {
-            private static void DisplayMenu()
-
-            {
-
-                Console.WriteLine("What do you want to do?");
-
-                Console.WriteLine("1 - Display all users");
-
-                Console.WriteLine("2 - Add a new user");
-
-                Console.WriteLine("3 - Get a user by message");
-
-                Console.WriteLine("4 - Delete a user by message");
-
-                Console.WriteLine("5 - Update a user");
-
-                Console.WriteLine("6 - Exit");
-
-            }
         }
     }
 }
---
+++ BoardLibrary/Person.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardLibrary
+{
+    public class Person
+    {
+
+        public virtual string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+
+            }
+
+        }
+        // ToString() is virtual in object class
+        // and every class in C# derives from object
+        public override string ToString()
+
+        {
+
+            return FullName;
+
+        }    
+        // default value is null
+        private string firstName;
+        public string FirstName
+        {
+            get
+            {
+
+                return firstName;
+
+            }
+            set
+            {
+                if (string.IsNullOrEmpty(value) == false)
+
+                {
+
+                    firstName = value;
+
+                }
+
+            }
+
+        }
+
+        public string LastName { get; set; }
+        // default value is 1/1/0001 12:00:00 AM
+        public DateTime BirthDate { get; private set; }
+        // default value is first enum value
+        /// <summary>
+        /// Computed property to determine the age of a person
+        /// </summary>
+        public double Age
+
+        {
+
+            get
+
+            {
+
+                return (DateTime.Now - this.BirthDate).TotalDays / 365.2425;
+
+            }
+
+        }
+        public Person() : this("Popescu", "Ion", DateTime.Now)
+        {
+
+
+
+        }
+
+
+
+        public Person(string firstName, string lastName, DateTime birthDate)
+
+        {
+
+            this.FirstName = firstName;
+
+            this.LastName = lastName;
+
+            this.BirthDate = birthDate;
+
+        }
+
+    }
+}
