6eb20eee768701c167a466bfb7cea424e67e0f2c
cimpoes.andrei.alexandru@gmail.com <cimpoes.andrei.alexandru@gmail.com>
2019-01-05 17:00:53 +0200

    Add User class

---
+++ BoardLibrary/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardLibrary
+{
+    public class User : Person
+    {
+
+        public string email { get; private set; }
+
+        // default value is null
+        public User(string email, string firstName, string lastName, DateTime birthDate)
+            : base(firstName, lastName, birthDate)
+        {
+
+            this.email = email;
+        }
+    }
+}
